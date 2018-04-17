c72f7ae0d3aa8b60532bca67e469a6e7dc7f81e7
Sauli Tähkäpää <saulis@reaktor.fi>
2018-04-17 13:58:58 +0300

    Awesome loading animation

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,20 +2,34 @@ import React, { Component } from 'react'
 import './button.css'
 
 class Button extends Component {
-
-    render() {
-        const { light, onClick, x, y } = this.props
-
-        return (
-            <circle
-                r={20}
-                cx={x}
-                cy={y}
-                className={ light.state.on ? 'switch on' : 'switch'}
-                onClick={() => onClick(light.id)(light.state.on)}
-            />
-        )
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false
     }
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ loading: false })
+  }
+
+  render() {
+    const { light, onClick, x, y } = this.props
+
+    return (
+      <circle
+        r={20}
+        cx={x}
+        cy={y}
+        className={`switch ${light.state.on ? 'on ' : ''} ${this.state.loading ? 'loading' : ''}`}
+        onClick={() => {
+          this.setState({ loading: true })
+          onClick(light.id)(light.state.on)
+        }
+        }
+      />
+    )
+  }
 }
 
 export default Button
